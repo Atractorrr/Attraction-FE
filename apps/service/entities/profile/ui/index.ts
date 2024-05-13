9c215c3b8240e8e859799754d8003b5e20b35790
woogie0303 <rkdehddnr15123@gmail.com>
2024-05-13 18:27:47 +0900

    feat: 유저 정보 컴포넌트 (#ATR-28)

--- apps/service/entities/profile/ui/index.ts
+++ apps/service/entities/profile/ui/index.ts
@@ -1,5 +1,13 @@
 import PreferTagItem from './PreferTagItem'
 import PreferTagList from './PreferTagList'
 import ProfileImage from './ProfileImage'
+import ProfileBackground from './ProfileBackground'
+import UserInfo from './UserInfo'
 
-export { PreferTagItem, PreferTagList, ProfileImage }
+export {
+  PreferTagItem,
+  PreferTagList,
+  ProfileImage,
+  ProfileBackground,
+  UserInfo,
+}
