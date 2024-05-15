572715f660071f32a8403d9a01477b38a5e59c52
SWARVY <swarvy0826@naver.com>
2024-05-15 20:48:17 +0900

    chore: config 의존성 연동 및 preset 적용 (#ATR-55)

--- packages/design-system/tailwind.config.js
+++ packages/design-system/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  presets: [require('@attraction/config/attraction-preset')],
+  darkMode: 'selector',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
