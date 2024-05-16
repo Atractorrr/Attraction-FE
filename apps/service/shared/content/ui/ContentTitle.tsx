74c81a934a877edc7785a8ee366254404e624e87
woogie0303 <rkdehddnr15123@gmail.com>
2024-05-16 21:05:38 +0900

    feat: line clamp 기능 추가 (#ATR-26)

--- apps/service/shared/content/ui/ContentTitle.tsx
+++ apps/service/shared/content/ui/ContentTitle.tsx
@@ -4,7 +4,7 @@ import { TitleType } from '../model/types/title'
 export default function ContentTitle({ type, content }: TitleType) {
   return (
     <div
-      className={`${type === 'main' ? 'font-medium' : 'text-sm text-[#6F7A86]'} self-center`}>
+      className={`${type === 'main' ? 'font-medium' : 'text-sm text-[#6F7A86]'} line-clamp-2 self-center`}>
       {content}
     </div>
   )
