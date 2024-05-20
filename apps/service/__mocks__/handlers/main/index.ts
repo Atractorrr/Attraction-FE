c6afc694a583064aacf0cfa4c77782a06797e117
SWARVY <swarvy0826@naver.com>
2024-05-20 23:10:18 +0900

    feat: msw mock data 생성 및 적용 (#ATR-59)

--- apps/service/__mocks__/handlers/main/index.ts
+++ apps/service/__mocks__/handlers/main/index.ts
@@ -1,5 +1,34 @@
-import { HttpHandler } from 'msw'
+import { HttpHandler, HttpResponse } from 'msw'
+import { get, getParams } from '../tools'
+import { recentArticles, trendNewsLetters } from '@/__mocks__/data'
 
-const mainHandlers: HttpHandler[] = []
+const mainHandlers: HttpHandler[] = [
+  get('/v1/articles/recent', ({ request }) => {
+    const url = new URL(request.url)
+    const size = Number(url.searchParams.get('size') ?? 5)
+    const res = {
+      mainArticles: recentArticles.slice(0, size),
+    }
+    return HttpResponse.json(res)
+  }),
+  get('/v1/newsletters/recommend', ({ request }) => {
+    const priorityCategory = ['IT', '디자인']
+    const url = new URL(request.url)
+    const size = Number(url.searchParams.get('size' ?? 10))
+    const category = url.searchParams.get('category')
+    const res = {
+      priorityCategory,
+      content: {
+        category,
+        isUser: true,
+        newsletters: category
+          ? trendNewsLetters[category].slice(0, size)
+          : trendNewsLetters['RECOMMEND'].slice(0, size),
+      },
+    }
+
+    return HttpResponse.json(res)
+  }),
+]
 
 export default mainHandlers
