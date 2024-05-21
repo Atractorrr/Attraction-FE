249860a22ac3ac20787be7dcb294c18c77de8031
SWARVY <swarvy0826@naver.com>
2024-05-21 11:27:59 +0900

    feat: 랭킹 섹션 더보기 텍스트 버튼 추가 (#ATR-21)

--- apps/service/widgets/userRank/ui/index.tsx
+++ apps/service/widgets/userRank/ui/index.tsx
@@ -7,7 +7,10 @@ export default function UserRank() {
   return (
     <Background>
       <div className="grid w-full items-start gap-y-4">
-        <Title icon={<TrophyOutline className="size-5" />} text="유저 랭킹" />
+        <div className="flex justify-between">
+          <Title icon={<TrophyOutline className="size-5" />} text="유저 랭킹" />
+          <a className="text-sm text-gray-400">더보기</a>
+        </div>
         <div className="grid justify-items-center gap-y-4 p-5">
           <Image
             src="/images/warning.png"
@@ -17,7 +20,9 @@ export default function UserRank() {
           />
           <div className="grid gap-y-2 text-center">
             <p className="text-lg font-bold">랭킹 시스템을 준비하고 있어요</p>
-            <p>조금만 기다려주시면 좋은 서비스로 돌아올게요</p>
+            <p className="font-thin text-gray-400">
+              조금만 기다려주시면 좋은 서비스로 돌아올게요
+            </p>
           </div>
         </div>
       </div>
