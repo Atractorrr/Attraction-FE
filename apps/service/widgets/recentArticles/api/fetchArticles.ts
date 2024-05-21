34344e613a9b19656e346acaeaeb6e93d4b929c6
SWARVY <swarvy0826@naver.com>
2024-05-21 11:24:49 +0900

    feat: 최근 받은 뉴스레터 컴포넌트 기능 구현 (#ATR-19)

---
+++ apps/service/widgets/recentArticles/api/fetchArticles.ts
@@ -0,0 +1,12 @@
+import { RecentArticleReponse } from '../model'
+
+export async function fetchArticles(
+  size: number = 5,
+): Promise<RecentArticleReponse> {
+  const apiURL = new URL(`${process.env.API_URL}/api/v1/articles/recent`)
+  apiURL.searchParams.set('size', size.toString())
+
+  const res = await fetch('http://localhost:3000/api/v1/articles/recent?size=5')
+
+  return res.json()
+}
---
+++ apps/service/widgets/recentArticles/model/types/article.ts
@@ -0,0 +1,14 @@
+export interface RecentArticle {
+  id: number
+  newsletterThumbnailUrl: string
+  newsletterTitle: string
+  articleThumbnailUrl: string
+  title: string
+  date: Date
+  readingTime: number
+  readPercentage: number
+}
+
+export interface RecentArticleReponse {
+  mainArticles: RecentArticle[]
+}
