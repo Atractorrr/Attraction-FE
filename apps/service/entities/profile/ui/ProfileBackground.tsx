23e8e9c01db9a8c2b77663c5acd9f53136451934
woogie0303 <rkdehddnr15123@gmail.com>
2024-05-21 10:57:26 +0900

    feat: 프로필 배경 업로드 기능 구현 (#ATR-77)

--- apps/service/entities/profile/ui/ProfileBackground.tsx
+++ apps/service/entities/profile/ui/ProfileBackground.tsx
@@ -7,10 +7,8 @@ type Props = {
 
 export default function ProfileImage({ imgSrc }: Props) {
   return (
-    <div className="px-5 pt-5">
-      <div className="relative h-64 w-full overflow-hidden rounded-md">
-        <Image alt="배경사진" src={imgSrc} fill className="object-cover" />
-      </div>
+    <div className="relative h-64 w-full overflow-hidden rounded-md">
+      <Image alt="배경사진" src={imgSrc} fill className="object-cover" />
     </div>
   )
 }
--- apps/service/features/profile/ui/index.tsx
+++ apps/service/features/profile/ui/index.tsx
@@ -1,21 +1,71 @@
 'use client'
 
-import React from 'react'
 import { Button } from '@attraction/design-system'
+import { FloppyDiskEmoji } from '@attraction/icons'
 import * as Entities from '@/entities'
 import { UserProfileType } from '../model/types'
+import { useImgUpload } from '../libs/hooks/useImgUpload'
 
 type Props = {
   userProfile: UserProfileType
 }
 
 export default function Profiles({ userProfile }: Props) {
+  // TODO: 프로필도 지우기, 비디오기능 안받기 (백엔드랑 api맞출때 같이 하기)
+  const {
+    fileInputRef: profileInputRef,
+    imgSrc: profileImgSrc,
+    fileUploadHandler: profileUploadHandler,
+  } = useImgUpload()
+  const {
+    fileInputRef: backgroundInputRef,
+    imgSrc: backgroundImgSrc,
+    fileUploadHandler: backgroundUploadHandler,
+  } = useImgUpload()
+
   return (
     <div className="relative flex w-full flex-col rounded-lg bg-white">
-      <Entities.Profile.ProfileBackground imgSrc={userProfile.backgroundImg} />
+      <div className="group relative px-5 pt-5">
+        <Entities.Profile.ProfileBackground
+          imgSrc={backgroundImgSrc || userProfile.backgroundImg}
+        />
+        <input
+          type="file"
+          ref={backgroundInputRef}
+          hidden
+          onChange={backgroundUploadHandler}
+        />
+        <Button
+          type="button"
+          className="invisible absolute right-2 top-8 h-fit  items-center rounded-xl bg-slate-100 p-1 opacity-0 transition-all
+          group-hover:visible group-hover:right-8 group-hover:opacity-100"
+          onClick={() => {
+            backgroundInputRef.current?.click()
+          }}>
+          <FloppyDiskEmoji className="inline-block size-8 rotate-[25deg]" />
+        </Button>
+      </div>
       <div className="flex size-full flex-col pl-5 md:flex-row md:pl-14">
-        <div className="size-20 md:size-auto">
-          <Entities.Profile.ProfileImage imaSrc={userProfile.profileImg} />
+        <div className="relative size-20 md:size-auto">
+          <div className="group relative size-40 shrink-0 -translate-y-1/2 rounded-full bg-white p-2">
+            <Entities.Profile.ProfileImage
+              imaSrc={profileImgSrc || userProfile.profileImg}
+            />
+            <Button
+              type="button"
+              className="invisible absolute bottom-3 right-0 h-fit -translate-y-2 rounded-xl bg-slate-100 p-1 shadow-2xl transition-all group-hover:visible  group-hover:translate-y-0"
+              onClick={() => {
+                profileInputRef.current?.click()
+              }}>
+              <FloppyDiskEmoji className="inline size-8" />
+            </Button>
+          </div>
+          <input
+            type="file"
+            ref={profileInputRef}
+            hidden
+            onChange={profileUploadHandler}
+          />
         </div>
         <div className="w-full p-5">
           <Entities.Profile.UserInfo
