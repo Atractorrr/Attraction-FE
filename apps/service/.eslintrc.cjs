3c6d0185114d0e22d7c589bd8bee18d9ffe817f4
SWARVY <swarvy0826@naver.com>
2024-05-23 11:38:50 +0900

    chore: eslint 설정 업데이트 (#ATR-95)

--- apps/service/.eslintrc.cjs
+++ apps/service/.eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['../../.eslintrc.cjs', 'next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json'],
     tsconfigRootDir: __dirname,
