3561bd84cf55cb097f6ce14a1bc0be95e96f8d1c
SWARVY <swarvy0826@naver.com>
2024-05-24 09:32:00 +0900

    refactor: 반응형 레이아웃 수정 및 transition, hover 효과 부여 (#ATR-114)

--- apps/service/app/page.tsx
+++ apps/service/app/page.tsx
@@ -5,13 +5,13 @@ import { UserRank } from '@/widgets/user-rank'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <section className="flex flex-col gap-5 lg:flex-row">
-        <div className="grid max-w-3xl justify-items-center gap-y-5 lg:max-w-4xl">
+    <main className="flex items-center justify-center lg:p-6">
+      <section className="grid max-w-6xl gap-6 lg:grid-cols-3">
+        <div className="flex w-full flex-col gap-y-5 lg:col-span-2">
           <RecentArticlesContainer />
           <TrendNewsletters />
         </div>
-        <div className="grid max-w-3xl justify-items-center gap-y-5 lg:max-w-md">
+        <div className="flex w-full flex-col justify-items-center gap-y-5">
           <UserRank />
           <MainAds />
         </div>
--- apps/service/widgets/user-rank/ui/UserRank.tsx
+++ apps/service/widgets/user-rank/ui/UserRank.tsx
@@ -6,25 +6,32 @@ import { Background, Title } from '@/shared/ui'
 export default function UserRank() {
   return (
     <Background>
-      <div className="grid w-full items-start gap-y-4">
-        <div className="flex justify-between">
+      <div className="flex min-h-[540px] w-full flex-col items-center gap-y-4">
+        <div className="flex w-full items-center justify-between">
           <Title icon={<TrophyOutline className="size-5" />} text="유저 랭킹" />
-          <Link href="/" className="text-sm text-gray-400">
+          <Link
+            href="/"
+            className="text-sm text-gray-400 transition-colors hover:text-blue-400">
             더보기
           </Link>
         </div>
-        <div className="grid justify-items-center gap-y-4 p-5">
-          <Image
-            src="/images/warning.png"
-            width={240}
-            height={240}
-            alt="warning"
-          />
-          <div className="grid gap-y-2 text-center">
-            <p className="text-lg font-bold">랭킹 시스템을 준비하고 있어요</p>
-            <p className="font-thin text-gray-400">
-              조금만 기다려주시면 좋은 서비스로 돌아올게요
-            </p>
+        <div className="grid h-full items-center justify-items-center">
+          <div className="grid justify-items-center gap-y-4 p-5">
+            <div className="size-52">
+              <Image
+                className="size-full"
+                src="/images/warning.png"
+                width={240}
+                height={240}
+                alt="warning"
+              />
+            </div>
+            <div className="grid gap-y-2 text-center">
+              <p className="text-lg font-bold">랭킹 시스템을 준비하고 있어요</p>
+              <p className="text-sm font-thin text-gray-400">
+                조금만 기다려주시면 좋은 서비스로 돌아올게요
+              </p>
+            </div>
           </div>
         </div>
       </div>
