cc293a8cf99b52a90c4191b9b6df38feab9a6332
woogie0303 <rkdehddnr15123@gmail.com>
2024-05-25 23:34:23 +0900

    feat: 회원가입 슬라이드 기능구현 (#ATR-137)

--- apps/service/widgets/sign-up-container/ui/SignUp.tsx
+++ apps/service/widgets/sign-up-container/ui/SignUp.tsx
@@ -1,11 +1,25 @@
 'use client'
 
-import React from 'react'
-import { FormProvider, useForm } from 'react-hook-form'
+import React, { useEffect, useState } from 'react'
+import { FormProvider, useForm, useFormState } from 'react-hook-form'
 import { Button } from '@attraction/design-system'
-import { UserInfoField, SignUpFormType } from '@/features/sign-up'
+import {
+  UserInfoField,
+  SignUpFormType,
+  UserJobField,
+  UserPreferTagField,
+} from '@/features/sign-up'
 
 export default function SignUp() {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [activeBtn, setActiveBtn] = useState(false)
+  const [checkError, setCheckError] = useState(false)
+
+  const signUpFieldArr = [
+    <UserInfoField key={0} />,
+    <UserJobField key={1} />,
+    <UserPreferTagField key={2} />,
+  ]
   const formMethod = useForm<SignUpFormType>({
     defaultValues: {
       email: '',
@@ -28,19 +42,48 @@ export default function SignUp() {
     },
   })
 
-  // const onSubmit = (data) => {
-  //   console.log(data)
-  // }
+  const { errors } = useFormState({ control: formMethod.control })
+
+  const onSubmit = () => {
+    if (activeIndex === 1 && formMethod.getValues('occupation') === '') {
+      formMethod.setError('occupation', { message: '직업을 선택해주세요' })
+    }
+  }
+
+  useEffect(() => {
+    if (activeBtn) {
+      setCheckError(true)
+    }
+  }, [activeBtn, errors])
+
+  useEffect(() => {
+    if (
+      checkError &&
+      !Object.keys(errors).length &&
+      activeIndex < signUpFieldArr.length - 1
+    ) {
+      setActiveIndex((pre) => pre + 1)
+      setActiveBtn(false)
+      setCheckError(false)
+    }
+  }, [activeIndex, checkError, errors, signUpFieldArr.length])
 
   return (
     <FormProvider {...formMethod}>
-      <form className="w-fit rounded-3xl bg-white p-10">
-        <UserInfoField />
+      <form
+        className="max-w-[540px] rounded-3xl bg-white p-10"
+        onSubmit={formMethod.handleSubmit(onSubmit)}>
+        {signUpFieldArr[activeIndex]}
         <Button
           type="submit"
           disabled={!!Object.keys(formMethod.formState.errors).length}
-          className="mt-14 w-full rounded-xl bg-gray-700 py-5 font-medium text-white">
-          다음으로
+          onClick={() => {
+            setActiveBtn(true)
+          }}
+          className={`mt-14 w-full rounded-xl bg-gray-700 py-5 font-medium text-white ${Object.keys(formMethod.formState.errors).length ? 'opacity-40' : 'opacity-100'}`}>
+          {activeIndex === signUpFieldArr.length - 1
+            ? '가입 할래요!'
+            : '다음으로'}
         </Button>
       </form>
     </FormProvider>
