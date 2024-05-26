04a725cff72ea6d0c79d6b2f894d9e54a99ebb6f
woogie0303 <rkdehddnr15123@gmail.com>
2024-05-26 18:52:14 +0900

    feat: 중복확인 기능 추가 (#ATR-149)

--- apps/service/__mocks__/handlers/index.ts
+++ apps/service/__mocks__/handlers/index.ts
@@ -3,6 +3,7 @@ import { get } from './tools'
 import inboxHandlers from './inbox'
 import mainHandlers from './main'
 import mypageHandlers from './mypage'
+import signUpHandlers from './sign-up'
 
 const handlers: HttpHandler[] = [
   get('/test', () => {
@@ -11,6 +12,7 @@ const handlers: HttpHandler[] = [
   ...mainHandlers,
   ...inboxHandlers,
   ...mypageHandlers,
+  ...signUpHandlers,
 ]
 
 export default handlers
--- apps/service/features/sign-up/ui/UserInfoField.tsx
+++ apps/service/features/sign-up/ui/UserInfoField.tsx
@@ -1,15 +1,46 @@
 import { useFormContext } from 'react-hook-form'
+import { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
 import { SignUpFormType } from '../model'
-
 import UserInfoExpirationDate from './UserInfoExpirationDate'
 import UserAgreement from './UserAgreement'
 
+const postDuplicateName = async (nickName: string) => {
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/join/username-duplicate`,
+    {
+      method: 'POST',
+      body: JSON.stringify(nickName),
+    },
+  ).then((res) => res.json())
+
+  return data
+}
+
 export default function UserInfoField() {
-  const duplicateCheckHandler = () => {}
+  const [isChecked, setIsChecked] = useState(false)
+  const { mutateAsync } = useMutation({
+    mutationFn: postDuplicateName,
+  })
   const {
     register,
     formState: { errors },
+    getValues,
+    setError,
+    clearErrors,
   } = useFormContext<SignUpFormType>()
+
+  const duplicateCheckHandler = async () => {
+    const data = await mutateAsync(getValues('nickName'))
+
+    if (data.message === '409 (MSW)') {
+      setError('nickName', { message: '중복된 이메일 입니다' })
+      setIsChecked(false)
+    } else {
+      setIsChecked(true)
+      clearErrors('nickName')
+    }
+  }
 
   return (
     <fieldset>
@@ -23,6 +54,14 @@ export default function UserInfoField() {
             id="nickName"
             className="grow rounded-lg border border-gray-100 p-2"
             placeholder="서비스에서 사용할 닉네임을 입력해 주세요"
+            {...register('nickName', {
+              onChange: () => {
+                setIsChecked(false)
+              },
+              validate: () => {
+                return '중복 확인을 해주세요'
+              },
+            })}
           />
           <button
             type="button"
@@ -31,6 +70,12 @@ export default function UserInfoField() {
             중복확인
           </button>
         </div>
+        {errors.nickName?.message && (
+          <p className="mt-2 text-red-500">{errors.nickName.message}</p>
+        )}
+        {isChecked && (
+          <p className="mt-2 text-green-500">사용가능한 닉네임 입니다</p>
+        )}
       </label>
       <label htmlFor="birth" aria-label="생년월일" className="mb-6 block">
         <p className="mb-2 text-sm text-gray-700">생년월일</p>
