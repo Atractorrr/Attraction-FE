565dc43dc654f38245b73d491a213449b71c642e
SWARVY <swarvy0826@naver.com>
2024-05-29 11:10:13 +0900

    chore: next svg 사용 설정하기 (#ATR-178)

--- apps/service/next.config.mjs
+++ apps/service/next.config.mjs
@@ -28,6 +28,14 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 export default nextConfig
