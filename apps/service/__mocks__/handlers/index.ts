204c4c189591c06eaa8370563f2952a80e5dddcf
SWARVY <swarvy0826@naver.com>
2024-05-30 00:21:23 +0900

    feat: 뉴스레터 소개 관련 msw 목데이터 생성 (#ATR-189)

--- apps/service/__mocks__/handlers/index.ts
+++ apps/service/__mocks__/handlers/index.ts
@@ -4,11 +4,13 @@ import inboxHandlers from './inbox'
 import mainHandlers from './main'
 import mypageHandlers from './mypage'
 import signInHandlers from './sign-in'
+import newsletterHandlers from './newsletter'
 
 const handlers: HttpHandler[] = [
   get('/test', () => {
     return HttpResponse.json({ message: `success)` })
   }),
+  ...newsletterHandlers,
   ...signInHandlers,
   ...mainHandlers,
   ...inboxHandlers,
