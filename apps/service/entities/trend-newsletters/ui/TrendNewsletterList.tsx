96028c437ee576cc42d16ea5e082f4d735f6ae0b
SWARVY <swarvy0826@naver.com>
2024-05-30 16:26:30 +0900

    fix: trendNewsletter api 변경사항 적용 (#ATR-187)

--- apps/service/entities/trend-newsletters/ui/TrendNewsletterList.tsx
+++ apps/service/entities/trend-newsletters/ui/TrendNewsletterList.tsx
@@ -1,16 +1,12 @@
 import { TrendNewsletterResponse } from '../model'
 import TrendNewsletterItem from './TrendNewsletterItem'
 
-interface TrendNewsletterListProps {
-  content: TrendNewsletterResponse['content']
-}
-
 export default function TrendNewsletterList({
-  content,
-}: TrendNewsletterListProps) {
+  mainPageNewsletters,
+}: TrendNewsletterResponse) {
   return (
     <section className="grid gap-4 xl:grid-cols-2">
-      {content.newsletters.map((newsletter) => (
+      {mainPageNewsletters.map((newsletter) => (
         <TrendNewsletterItem key={newsletter.id} newsletter={newsletter} />
       ))}
     </section>
