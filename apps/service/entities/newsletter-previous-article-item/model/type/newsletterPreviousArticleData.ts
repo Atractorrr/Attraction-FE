6c41e0c9f2e3daa16b3fc0656641deddf3a26d39
SWARVY <swarvy0826@naver.com>
2024-06-02 17:00:16 +0900

    feat: 변경된 API 사양에 맞추어 로직 변경 (#ATR-233)

---
+++ apps/service/entities/newsletter-previous-article-item/model/type/newsletterPreviousArticleData.ts
@@ -0,0 +1,10 @@
+export default interface NewsletterPreviousArticleData {
+  id: number
+  title: string
+  thumbnailUrl: string
+  contentUrl: string
+  contentSummary: string
+  readingTime: number
+  receivedAt: string
+  newsletterName: string
+}
