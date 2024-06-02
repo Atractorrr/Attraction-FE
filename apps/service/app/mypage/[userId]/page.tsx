fbf4814a13daef87523544a60f035ac0d0be08f6
woogie0303 <rkdehddnr15123@gmail.com>
2024-06-02 17:02:18 +0900

    feat: 데이터 없을 때 UI 구성 (#ATR-104)

--- apps/service/app/mypage/[userId]/page.tsx
+++ apps/service/app/mypage/[userId]/page.tsx
@@ -1,9 +1,6 @@
 import { UserRecord } from '@/widgets/user-record'
 import { SubscribeList, fetchSubscribeList } from '@/features/subscribe-list'
-import {
-  ProfileContainer,
-  fetchUserProfile,
-} from '@/features/profile-container'
+import { ProfileContainer } from '@/features/profile-container'
 import {
   RecentNewsletterContainer,
   fetchNewsletterList,
@@ -15,15 +12,14 @@ interface MyPageProps {
 
 export default async function MyPage({ params }: MyPageProps) {
   // TODO: 사이즈 커질때 배치 신경(max-width 설정)
-  const [userProfile, recentNewLetterList, subscribeList] = await Promise.all([
-    fetchUserProfile(params.userId),
+  const [recentNewLetterList, subscribeList] = await Promise.all([
     fetchNewsletterList(params.userId),
     fetchSubscribeList(params.userId),
   ])
 
   return (
     <div className="w-full">
-      <ProfileContainer userProfile={userProfile} />
+      <ProfileContainer userId={params.userId} />
       <UserRecord userId={params.userId} />
       <div className="mt-6 flex flex-col items-stretch justify-start gap-6 lg:flex-row lg:justify-between">
         <div className="h-auto w-full lg:w-2/3">
