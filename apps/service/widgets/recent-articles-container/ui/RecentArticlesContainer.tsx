9e9e882070f340a93baeb7fb52af29cdfda197fa
SWARVY <swarvy0826@naver.com>
2024-06-02 22:42:10 +0900

    feat: mainPageArticles가 없는 경우 핸들링 (#ATR-236)

--- apps/service/widgets/recent-articles-container/ui/RecentArticlesContainer.tsx
+++ apps/service/widgets/recent-articles-container/ui/RecentArticlesContainer.tsx
@@ -4,55 +4,79 @@ import Link from 'next/link'
 import { ChevronRightOutline, ClockOutline } from '@attraction/icons'
 import { RecentArticles } from '@/features/recent-articles'
 
-import { Background, LoadingSpinner, Title } from '@/shared/ui'
+import { Background, ErrorGuideTxt, LoadingSpinner, Title } from '@/shared/ui'
 import { useRecentArticles } from '@/features/recent-articles/model'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 
 interface RecentArticlesContainerProps {
   email: string | undefined
 }
 
+function LoginView({ email }: RecentArticlesContainerProps) {
+  const { data, isLoading } = useRecentArticles(email)
+
+  return (
+    <Background>
+      <div className="flex w-full flex-col gap-y-4 p-5">
+        <div className="flex h-fit w-full items-center justify-between">
+          <Title
+            icon={<ClockOutline className="size-5" />}
+            text="최근 받은 아티클"
+          />
+          <Link
+            href="/inbox"
+            className="text-sm text-gray-400 transition-colors  dark:hover:text-blue-300">
+            보관함 바로가기
+          </Link>
+        </div>
+        {isLoading ? <LoadingSpinner /> : null}
+        {data ? (
+          <RecentArticles mainPageArticles={data.mainPageArticles} />
+        ) : null}
+      </div>
+    </Background>
+  )
+}
+
+function NonLoginView() {
+  return (
+    <div className="px-5 md:px-0">
+      <div
+        className="flex min-h-[294px] flex-col items-center justify-center gap-y-10 rounded-xl border-gray-100 p-10 md:border"
+        style={{
+          backgroundImage: 'url(/images/introduce-background.jpg)',
+        }}>
+        <div className="flex flex-col text-center text-[28px] font-bold dark:text-black">
+          <p>뉴스레터의 모든 것</p>
+          <p>어트렉션에서 쉽고 간편하게</p>
+        </div>
+        <Link
+          href="/sign-in"
+          className="flex w-fit items-center justify-between gap-x-4 rounded-lg bg-green-500 px-4 py-2 text-white">
+          <p>로그인 하러갈래요!</p>
+          <ChevronRightOutline className="size-5" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
 export default function RecentArticlesContainer({
   email,
 }: RecentArticlesContainerProps) {
-  const { data, isPending } = useRecentArticles(email)
-
   return (
     <div>
       {email ? (
-        <Background>
-          <div className="flex w-full flex-col gap-y-4 p-5">
-            <div className="flex h-fit w-full items-center justify-between">
-              <Title
-                icon={<ClockOutline className="size-5" />}
-                text="최근 받은 아티클"
-              />
-              <Link
-                href="/inbox"
-                className="text-sm text-gray-400 transition-colors  dark:hover:text-blue-300">
-                보관함 바로가기
-              </Link>
-            </div>
-            {isPending ? <LoadingSpinner /> : null}
-            {data ? <RecentArticles mainArticles={data.mainArticles} /> : null}
-          </div>
-        </Background>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary onReset={reset} FallbackComponent={ErrorGuideTxt}>
+              <LoginView email={email} />
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       ) : (
-        <div
-          className="flex min-h-[294px] flex-col items-center justify-center gap-y-10 border-gray-100 p-10 md:rounded-xl md:border"
-          style={{
-            backgroundImage: 'url(/images/introduce-background.jpg)',
-          }}>
-          <div className="flex flex-col text-center text-[28px] font-bold dark:text-black">
-            <p>뉴스레터의 모든 것</p>
-            <p>어트렉션에서 쉽고 간편하게</p>
-          </div>
-          <Link
-            href="/sign-in"
-            className="flex w-fit items-center justify-between gap-x-4 rounded-lg bg-green-500 px-4 py-2 text-white">
-            <p>로그인 하러갈래요!</p>
-            <ChevronRightOutline className="size-5" />
-          </Link>
-        </div>
+        <NonLoginView />
       )}
     </div>
   )
