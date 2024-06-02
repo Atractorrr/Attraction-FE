9cefd3807aea25c2f07f534737c0ae5ec382b2e0
SWARVY <swarvy0826@naver.com>
2024-06-02 17:03:55 +0900

    refactor: newsletter previous article 항목 error boundary & suspense 적용 (#ATR-228)

--- apps/service/features/newsletter-previous-articles/ui/NewsletterPreviousArticles.tsx
+++ apps/service/features/newsletter-previous-articles/ui/NewsletterPreviousArticles.tsx
@@ -1,32 +1,69 @@
 'use client'
 
-import { Background, LoadingSpinner, Title } from '@/shared/ui'
+import { Suspense } from 'react'
+import {
+  Background,
+  ErrorGuideTxt,
+  GuideTxt,
+  LoadingSpinner,
+  Title,
+} from '@/shared/ui'
 import { ClockOutline } from '@attraction/icons'
-import NewsletterPreviousArticle from '@/entities/newsletter-previous-article-item/ui/NewsletterPreviousArticle'
+import { NewsletterPreviousArticleItem } from '@/entities/newsletter-previous-article-item'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
+import { ErrorBoundary } from 'react-error-boundary'
 import { useNewsletterPreviousArticles } from '../lib'
 
-interface NewsletterIntroduceProps {
+interface NewsletterPreviousArticlesProps {
   newsletterId: string
+}
+
+function NewsletterPreviousArticlesContent({
+  newsletterId,
+}: NewsletterPreviousArticlesProps) {
+  const { data } = useNewsletterPreviousArticles(newsletterId)
+
+  return (
+    <div>
+      {!data.data.length ? (
+        <div className="p-16">
+          <GuideTxt
+            title="지난 아티클이 없습니다"
+            sub="새로운 아티클을 기대해주세요!"
+          />
+        </div>
+      ) : (
+        <div className="flex w-full flex-col gap-y-5">
+          {data.data.map((newsletter) => (
+            <NewsletterPreviousArticleItem
+              key={newsletter.id}
+              {...newsletter}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default function NewsletterPreviousArticles({
   newsletterId,
-}: NewsletterIntroduceProps) {
-  const { data, isPending } = useNewsletterPreviousArticles(newsletterId)
-
+}: NewsletterPreviousArticlesProps) {
   return (
     <Background>
       <div className="grid w-full gap-y-5 p-5">
         <Title icon={<ClockOutline className="size-6" />} text="지난 아티클" />
-        {isPending || !data ? (
-          <LoadingSpinner />
-        ) : (
-          <div className="flex w-full flex-col gap-y-5">
-            {data.data.map((newsletter) => (
-              <NewsletterPreviousArticle key={newsletter.id} {...newsletter} />
-            ))}
-          </div>
-        )}
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary onReset={reset} FallbackComponent={ErrorGuideTxt}>
+              <Suspense fallback={<LoadingSpinner />}>
+                <NewsletterPreviousArticlesContent
+                  newsletterId={newsletterId}
+                />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
       </div>
     </Background>
   )
