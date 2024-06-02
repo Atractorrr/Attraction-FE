0d249234e506681c8e086f7d18c71f80ed6b0ab2
SWARVY <swarvy0826@naver.com>
2024-06-02 22:43:33 +0900

    feat: oauth 결과에 따라 회원가입창 / 메인으로 이동시키기 (#ATR-232)

--- apps/service/shared/constant/oAuthURL.ts
+++ apps/service/shared/constant/oAuthURL.ts
@@ -5,3 +5,7 @@
 export const GOOGLE_OAUTH_URL =
   new URL(`https://accounts.google.com/o/oauth2/auth?access_type=offline&approval_prompt=force&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_OAUTH_URL}&response_type=code&scope=email profile openid https://mail.google.com/%20https://www.googleapis.com/auth/gmail.readonly%20https://www.googleapis.com/auth/gmail.labels%20https://www.googleapis.com/auth/gmail.settings.basic%20https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile
 `)
+
+export const GOOGLE_OAUTH_FORCE_URL =
+  new URL(`https://accounts.google.com/o/oauth2/auth?access_type=offline&approval_prompt=force&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=https://main.d35lcnjadi8vq7.amplifyapp.com/api/auth/callback/google&response_type=code&scope=email profile openid https://mail.google.com/%20https://www.googleapis.com/auth/gmail.readonly%20https://www.googleapis.com/auth/gmail.labels%20https://www.googleapis.com/auth/gmail.settings.basic%20https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile
+`)
