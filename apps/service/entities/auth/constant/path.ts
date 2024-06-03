53d7b71d325339afd53fa0716afef3d031cfb2b6
ChanLee_KR <cl700120@gmail.com>
2024-06-03 17:01:53 +0900

    fix: 휴먼 에러 수정 (#ATR-219)

--- apps/service/entities/auth/constant/path.ts
+++ apps/service/entities/auth/constant/path.ts
@@ -1,4 +1,3 @@
 export const PUBLIC_PATH = ['/sign-in', '/sign-up']
 
-// export const PRIVATE_PATH = ['/inbox', '/mypage']
-export const PRIVATE_PATH = []
+export const PRIVATE_PATH = ['/inbox', '/mypage']
