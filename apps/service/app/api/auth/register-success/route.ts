b6c879fd1460f8f7f779b14887672ac604f7f73b
woogie0303 <rkdehddnr15123@gmail.com>
2024-06-04 11:38:46 +0900

    refactor: next response 수정 (#ATR-147)

--- apps/service/app/api/auth/register-success/route.ts
+++ apps/service/app/api/auth/register-success/route.ts
@@ -1,5 +1,7 @@
 import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
 export async function DELETE() {
   cookies().delete('notRegistered')
+  return NextResponse.redirect(process.env.NEXT_PUBLIC_FE_URL as string)
 }
