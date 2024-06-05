7bc0c0ed32a119387bf96ae60a299acc4b32d50e
ChanLee_KR <cl700120@gmail.com>
2024-06-05 11:40:07 +0900

    fix: rewrites 요청 에러 수정 (#ATR-256)

--- apps/service/entities/recent-article-item/ui/RecentArticleItem.tsx
+++ apps/service/entities/recent-article-item/ui/RecentArticleItem.tsx
@@ -12,21 +12,22 @@ export default function RecentArticleItem({ ...props }: RecentArticle) {
   return (
     <Link
       href={`/inbox/article/${props.id}`}
-      className="grid max-w-[280px] justify-items-start gap-y-4">
+      className="grid max-w-[280px] justify-items-start gap-y-4"
+      title={`아티클 보기: ${props.title}`}>
       <div className="relative h-40 w-full overflow-hidden rounded-xl">
         <Image
           className="size-full object-cover transition-all hover:scale-110"
           width={260}
           height={140}
-          src={props.articleThumbnailUrl}
-          alt={props.title}
+          src={props.articleThumbnailUrl || '/images/default-16x9.jpg'}
+          alt={`아티클 썸네일 이미지: ${props.title}`}
         />
         <div className="absolute bottom-2 right-2 rounded-md bg-black/60 p-1 text-xs text-white">
           {props.readingTime > 1 ? `약 ${props.readingTime}분` : `1분 미만`}
         </div>
         <div className="absolute bottom-0 left-0 mt-4 h-1 w-full overflow-hidden bg-gray-100">
           {/* // TODO: cva 적용하여 리팩토링 */}
-          <div
+          <span
             className="h-full bg-blue-400"
             style={{ width: `${props.readPercentage}%` }}
           />
@@ -38,13 +39,13 @@ export default function RecentArticleItem({ ...props }: RecentArticle) {
             className="size-8 rounded-full object-cover"
             width={100}
             height={100}
-            src={props.newsletterThumbnailUrl}
-            alt={props.title}
+            src={props.newsletterThumbnailUrl || '/images/default-1x1.jpg'}
+            alt={`뉴스레터 썸네일 이미지: ${props.newsletterTitle}`}
           />
         </div>
         <div className="flex flex-col">
-          <h2 className="grow break-keep">{props.title}</h2>
-          <div className="flex text-sm text-gray-500">
+          <h2 className="grow break-keep font-medium">{props.title}</h2>
+          <div className="flex text-sm text-gray-500 dark:text-gray-400">
             <p>{props.newsletterTitle}</p>
             <p className="before:mx-1 before:content-['·']">
               {dayjs(props.date).fromNow()}
