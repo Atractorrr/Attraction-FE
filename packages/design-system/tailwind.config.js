4f4087503c8bda153b87f7f05d14a503c01d193e
SWARVY <swarvy0826@naver.com>
2024-06-11 10:59:38 +0900

    feat: tsup 빌드 세팅 (#ATR-270)

--- packages/design-system/tailwind.config.js
+++ packages/design-system/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['./components/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
-  prefix: '',
+  content: ['./**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
