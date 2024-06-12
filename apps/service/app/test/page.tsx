78b2084ee206d9b7ad2db6333ae1be395ae4f663
ChanLee_KR <cl700120@gmail.com>
2024-06-12 17:13:49 +0900

    feat: 컬러시스템 색상 조정 (#ATR-290)

--- apps/service/app/test/page.tsx
+++ apps/service/app/test/page.tsx
@@ -5,7 +5,15 @@ import { Badge } from '@attraction/design-system/dist'
 export default function TestPage() {
   return (
     <div>
-      <Badge>asdf</Badge>
+      <Badge variant="active">active</Badge>
+      <Badge>default</Badge>
+      <Badge variant="red">red</Badge>
+      <Badge>default</Badge>
+      <Badge variant="yellow">yellow</Badge>
+      <Badge>default</Badge>
+      <Badge variant="green">green</Badge>
+      <Badge>default</Badge>
+      <Badge variant="blue">blue</Badge>
     </div>
   )
 }
