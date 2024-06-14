f61f97dac51a420cd81e3a41858224d8bb7083e9
SWARVY <swarvy0826@naver.com>
2024-06-14 12:37:15 +0900

    feat: 에러 텍스트 디자인 시안 반영 (#ATR-321)

--- apps/service/shared/ui/ErrorGuideTxt.tsx
+++ apps/service/shared/ui/ErrorGuideTxt.tsx
@@ -1,12 +1,28 @@
+import { RefreshOutline, SirenEmoji } from '@attraction/icons'
+import { Button } from '@attraction/design-system/dist'
 import GuideTxt from './GuideTxt'
 
-export default function ErrorGuideTxt() {
+interface ErrorGuideTxtProps {
+  title?: string
+  sub?: string
+}
+
+export default function ErrorGuideTxt({
+  title = '이용에 불편을 드려 죄송해요',
+  sub = '동일한 현상이 계속될 경우 문의 부탁드려요',
+}: ErrorGuideTxtProps) {
   return (
-    <div className="pb-40 pt-32">
-      <GuideTxt
-        title="이용에 불편을 드려 죄송해요"
-        sub="동일한 현상이 계속될 경우 문의 부탁드려요"
-      />
+    <div className="flex flex-col gap-y-9 bg-gray-100 pb-40 pt-32">
+      <div className="flex flex-col gap-y-6">
+        <div className="size-16 shrink-0 rounded-full ">
+          <SirenEmoji className="size-9" />
+        </div>
+        <GuideTxt title={title} sub={sub} />
+      </div>
+      <Button className="flex gap-x-2 rounded-md bg-gray-100">
+        <RefreshOutline />
+        <span>다시시도</span>
+      </Button>
     </div>
   )
 }
