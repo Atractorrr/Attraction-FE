bf2d8904c3f3c128b0ae6ab1bda7be89e0e3545d
SWARVY <swarvy0826@naver.com>
2024-06-15 13:31:45 +0900

    feat: checkbox 스토리 변경 (#ATR-336)

--- packages/design-system/src/Checkbox/Checkbox.stories.tsx
+++ packages/design-system/src/Checkbox/Checkbox.stories.tsx
@@ -9,20 +9,51 @@ const meta: Meta<typeof Checkbox> = {
   parameters: {
     componentSubtitle: 'Attraction에서 사용되는 체크박스 컴포넌트입니다.',
   },
+  argTypes: {
+    color: {
+      description: '체크박스의 색상을 지정합니다.',
+      control: 'select',
+      options: ['default', 'red', 'yellow', 'green', 'blue'],
+      table: {
+        type: { summary: 'default | red | yellow | green | blue' },
+        defaultValue: { summary: 'default' },
+      },
+    },
+    label: {
+      description: '체크박스 옆에 표시할 레이블을 입력합니다.',
+      control: 'text',
+      table: {
+        defaultValue: { summary: 'Badge' },
+      },
+    },
+  },
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const CheckboxDefault: Story = {
+  render: (args) => <Checkbox {...args} />,
+}
+
+export const CheckboxRed: Story = {
+  render: () => <Checkbox color="red" />,
+}
+
+export const CheckboxYellow: Story = {
+  render: () => <Checkbox color="yellow" />,
+}
+
+export const CheckboxGreen: Story = {
+  render: () => <Checkbox color="green" />,
+}
+
+export const CheckboxBlue: Story = {
+  render: () => <Checkbox color="blue" />,
+}
+
+export const CheckboxWithLabel: Story = {
   render: () => (
-    <div className="ds-flex ds-items-center ds-space-x-2">
-      <Checkbox id="terms" />
-      <label
-        htmlFor="terms"
-        className="ds-text-sm ds-font-medium ds-leading-none peer-disabled:ds-cursor-not-allowed peer-disabled:ds-opacity-70">
-        Accept terms and conditions
-      </label>
-    </div>
+    <Checkbox color="default" label="this is attraction checkbox" />
   ),
 }
