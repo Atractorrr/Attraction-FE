1333add1e41a9e02d2d5ec736ebfa859d97603b7
woogie0303 <rkdehddnr15123@gmail.com>
2024-06-16 12:40:16 +0900

    feat: 랭킹 시스템 api 맞추기 (#ATR-304)

--- apps/service/widgets/user-rank/api/getUserRanking.ts
+++ apps/service/widgets/user-rank/api/getUserRanking.ts
@@ -1,16 +1,21 @@
 import { UserRankingInfoType } from '../model'
 
-const getUserRanking = async (type: 'article' | 'strict') => {
+const getUserRanking = async (
+  type: 'article' | 'streak',
+): Promise<UserRankingInfoType[]> => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/rank/${type}`,
   )
+
+  const data = await res.json()
 
   if (!res.ok) {
     throw new Error('잘못된 데이터 입니다.')
   }
 
-  // TODO: 받아오는 키 값에따라 데이터 분류하기 타입스크립트 관련
-
-  return res.json() as Promise<UserRankingInfoType>
+  if (Object.prototype.hasOwnProperty.call(data, 'userArticleRank')) {
+    return data.userArticleRank
+  }
+  return data.userStreakRank
 }
 export default getUserRanking
--- apps/service/widgets/user-rank/ui/UserRank.tsx
+++ apps/service/widgets/user-rank/ui/UserRank.tsx
@@ -10,15 +10,13 @@ import { useState } from 'react'
 import { getUserRanking } from '../api'
 import UserRankMedal from './UserRankMedal'
 
-const mock = Array.from({ length: 10 })
-
 export default function UserRank() {
   // TODO: 쿼리키 바뀔 때 캐시 데이터 쓰는지 알아보기
 
-  const [activeRanking, setActiveRanking] = useState<'article' | 'strict'>(
+  const [activeRanking, setActiveRanking] = useState<'article' | 'streak'>(
     'article',
   )
-  const { data } = useQuery({
+  const { data: userRankingArr } = useQuery({
     queryKey: ['userRanking', activeRanking],
     queryFn: () => {
       return getUserRanking(activeRanking)
@@ -27,11 +25,11 @@ export default function UserRank() {
 
   return (
     <Background>
-      <div className="flex min-h-[540px] w-full flex-col p-5">
+      <div className="min-h-[540px] w-full p-5">
         <div className="mb-4 flex w-full items-center justify-between">
           <Title icon={<TrophyOutline className="size-5" />} text="유저 랭킹" />
         </div>
-        <div className="mb-5 flex">
+        <div className="mb-5 flex w-full">
           <Button
             className={`w-full border-b-2 py-3 ${activeRanking === 'article' ? 'border-gray-700 text-gray-700' : 'border-gray-100  text-gray-500'}`}
             onClick={() => {
@@ -40,34 +38,34 @@ export default function UserRank() {
             아티클
           </Button>
           <Button
-            className={`w-full border-b-2 py-3 ${activeRanking === 'strict' ? 'border-gray-700 text-gray-700' : 'border-gray-100  text-gray-500'}`}
+            className={`w-full border-b-2 py-3 ${activeRanking === 'streak' ? 'border-gray-700 text-gray-700' : 'border-gray-100  text-gray-500'}`}
             onClick={() => {
-              setActiveRanking('strict')
+              setActiveRanking('streak')
             }}>
             스트릭
           </Button>
         </div>
 
-        <div className="flex size-full h-[400px] flex-col gap-3 overflow-y-auto">
-          {mock.map((_, i) => {
+        <div className="size-full h-full overflow-y-auto pr-5 lg:h-[400px]">
+          {userRankingArr?.map((user, i) => {
             return (
-              <div className="flex gap-3">
+              <div className="peer flex w-full gap-3 peer-[]:mt-3">
                 <UserRankMedal rank={i} />
-                <div className="shrink-0">
+                <div className="size-10 shrink-0 rounded-full border border-gray-100 bg-gray-50 dark:border-gray-600 dark:bg-gray-700">
                   <Image
                     width={40}
                     height={40}
-                    src={data?.profileImg || '/images/default-4x1.jpg'}
+                    src={user?.profileImg || '/images/default-1x1.jpg'}
                     className="rounded-full"
                     alt="프로필사진"
                   />
                 </div>
-                <div className="">
-                  <p className="font-medium text-black dark:text-white">
-                    {i + 1}위 {data?.nickname}
+                <div className="w-full max-w-[calc(100%-3rem-40px)]">
+                  <p className="truncate font-medium">
+                    {i + 1}위 {user?.nickname}
                   </p>
                   <p className="text-sm text-gray-500">
-                    최장 {data?.value}일 연속 아티클을 읽었어요! 🎉
+                    최장 {user?.value}일 연속 아티클을 읽었어요! 🎉
                   </p>
                 </div>
               </div>
