a417b81312715a3dea99f68e85c2fc626676dd3c
SWARVY <swarvy0826@naver.com>
2024-06-17 13:26:13 +0900

    style: need login 컴포넌트에 개행 추가 (#ATR-359)

--- apps/service/entities/auth/ui/NeedLogin.tsx
+++ apps/service/entities/auth/ui/NeedLogin.tsx
@@ -13,6 +13,7 @@ interface NeedLoginProps {
 
 export default function NeedLogin({ title, sub }: NeedLoginProps) {
   const router = useRouter()
+
   return (
     <Container>
       <div className="flex min-h-96 w-full items-center justify-center px-5 pb-32 pt-20">
