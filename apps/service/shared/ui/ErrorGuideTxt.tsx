9c324ac1664e73824a4e54f8efd7c253892eb0b0
ChanLee_KR <cl700120@gmail.com>
2024-06-19 13:42:26 +0900

    feat: 스타일 검수 (#ATR-376)

--- apps/service/shared/ui/ErrorGuideTxt.tsx
+++ apps/service/shared/ui/ErrorGuideTxt.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { RefreshOutline } from '@attraction/icons'
 import { Button } from '@attraction/design-system'
-import { useRouter } from 'next/navigation'
 import GuideTxt from './GuideTxt'
 
 interface ErrorGuideTxtProps {
