e389334390abd47541bd5a75d9a523c2d2437676
SWARVY <swarvy0826@naver.com>
2024-06-22 01:54:09 +0900

    refactor: xs사이즈 대응하여 반응형 로직 추가 (#ATR-427)

--- apps/service/widgets/newsletter-introduce/ui/NewsletterSubscribeButton.tsx
+++ apps/service/widgets/newsletter-introduce/ui/NewsletterSubscribeButton.tsx
@@ -24,7 +24,7 @@ export default function NewsletterSubscribeButton({
 
   return (
     <Button
-      className="h-[40px] w-full rounded-lg bg-gray-700 py-2 text-center text-white md:w-[180px] dark:bg-white dark:text-gray-700"
+      className="mt-7 h-[40px] w-full rounded-lg bg-gray-700 py-2 text-center text-white sm:mt-0 dark:bg-white dark:text-gray-700"
       onClick={() => handleSubscribe()}>
       구독하기
     </Button>
