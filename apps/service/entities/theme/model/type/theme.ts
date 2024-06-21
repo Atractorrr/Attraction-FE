36e330add5625e1973abc8e745b67a1baf95fab5
SWARVY <swarvy0826@naver.com>
2024-06-22 02:18:39 +0900

    fix: 테마 타입 omit -> exclude로 수정 (#ATR-425)

--- apps/service/entities/theme/model/type/theme.ts
+++ apps/service/entities/theme/model/type/theme.ts
@@ -2,4 +2,4 @@ import { THEME_LIST } from '../../constant'
 
 export type Theme = keyof typeof THEME_LIST
 
-export type RealTheme = Omit<Theme, 'system'>
+export type RealTheme = Exclude<Theme, 'system'>
