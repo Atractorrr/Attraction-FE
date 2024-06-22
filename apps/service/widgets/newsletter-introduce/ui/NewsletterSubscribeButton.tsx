715e054b1bb093d5ca0b1930af4581d9758bf7c5
SWARVY <swarvy0826@naver.com>
2024-06-22 14:06:55 +0900

    refactor: 뉴스레터 소개 페이지 디자인 수정 (#ATR-420)

--- apps/service/widgets/newsletter-introduce/ui/NewsletterSubscribeButton.tsx
+++ apps/service/widgets/newsletter-introduce/ui/NewsletterSubscribeButton.tsx
@@ -24,7 +24,7 @@ export default function NewsletterSubscribeButton({
 
   return (
     <Button
-      className="mt-7 h-[40px] w-full rounded-lg bg-gray-700 py-2 text-center text-white sm:mt-0 dark:bg-white dark:text-gray-700"
+      className="mt-1 h-[40px] w-full rounded-lg bg-gray-700 py-2 text-center text-white md:mt-0 dark:bg-white dark:text-gray-700"
       onClick={() => handleSubscribe()}>
       구독하기
     </Button>
