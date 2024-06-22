8318dc80274d4cf9cc45905a77bba4992a67d9a1
SWARVY <swarvy0826@naver.com>
2024-06-23 00:28:45 +0900

    feat: sentry 적용 (#ATR-435)

---
+++ apps/service/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://dbdc1f5215cd2dfcbb69bec059ddd7ca@o4507476054179840.ingest.us.sentry.io/4507476055818240',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+})
