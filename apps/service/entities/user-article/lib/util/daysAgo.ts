7c7828e5663c241b77b8e1516c7e30535ecd7488
ChanLee_KR <cl700120@gmail.com>
2024-06-25 11:50:33 +0900

    fix: 아티클 삭제 시점 조정 및 스타일 검수 (#ATR-451)

--- apps/service/entities/user-article/lib/util/daysAgo.ts
+++
@@ -1,6 +0,0 @@
-export default function daysAgo(targetDate: string) {
-  const today = new Date().getTime()
-  const diff = Math.abs(today - new Date(targetDate).getTime())
-
-  return Math.ceil(diff / (1000 * 60 * 60 * 24))
-}
