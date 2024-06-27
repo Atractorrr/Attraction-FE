b25dd84232c569955f1fe66d556116a16c15fab5
ChanLee_KR <cl700120@gmail.com>
2024-06-27 15:27:57 +0900

    fix: 세션 요청에 credentials 옵션 추가 (#ATR-474)

--- apps/service/entities/auth/api/getUserSession.ts
+++ apps/service/entities/auth/api/getUserSession.ts
@@ -1,17 +1,15 @@
 'use server'
 
-import { headers } from 'next/headers'
 import type { UserSessionData, UserSessionResponse } from '../model'
 
 export default async function getUserSession(): Promise<{
-  header: Headers
   data: UserSessionData
 }> {
-  const header = headers()
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/v1/auth/session`,
     {
-      headers: header,
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
       cache: 'no-store',
       next: { revalidate: 0 },
     },
@@ -23,5 +21,5 @@ export default async function getUserSession(): Promise<{
 
   const { data }: UserSessionResponse = await res.json()
 
-  return { header: res.headers, data }
+  return { data }
 }
