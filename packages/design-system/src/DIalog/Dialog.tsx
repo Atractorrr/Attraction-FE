504e3720390f50f7a84acec56fefd42bd6d20e89
ChanLee_KR <cl700120@gmail.com>
2024-06-27 23:46:15 +0900

    feat: 체크박스 및 dialog 컴포넌트 디자인 수정 (#ATR-484)

--- packages/design-system/src/DIalog/Dialog.tsx
+++ packages/design-system/src/DIalog/Dialog.tsx
@@ -37,14 +37,14 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'ds-fixed ds-left-[50%] ds-top-[50%] ds-z-50 ds-grid ds-w-full ds-max-w-[100vw-2.5rem] ds-translate-x-[-50%] ds-translate-y-[-50%] ds-gap-4 ds-rounded-xl ds-border ds-border-gray-100 ds-bg-white ds-p-6 ds-shadow-lg ds-duration-200 data-[state=open]:ds-animate-in data-[state=closed]:ds-animate-out data-[state=closed]:ds-fade-out-0 data-[state=open]:ds-fade-in-0 data-[state=closed]:ds-zoom-out-95 data-[state=open]:ds-zoom-in-95 data-[state=closed]:ds-slide-out-to-left-1/2 data-[state=closed]:ds-slide-out-to-top-[48%] data-[state=open]:ds-slide-in-from-left-1/2 data-[state=open]:ds-slide-in-from-top-[48%] sm:ds-max-w-lg dark:ds-border-gray-800 dark:ds-bg-gray-800',
+        'ds-fixed ds-left-[50%] ds-top-[50%] ds-z-50 ds-grid ds-w-full ds-min-w-60 ds-max-w-[calc(100vw-2.5rem)] ds-translate-x-[-50%] ds-translate-y-[-50%] ds-gap-4 ds-rounded-xl ds-border ds-border-gray-100 ds-bg-white ds-p-5 ds-shadow-lg ds-duration-200 data-[state=open]:ds-animate-in data-[state=closed]:ds-animate-out data-[state=closed]:ds-fade-out-0 data-[state=open]:ds-fade-in-0 data-[state=closed]:ds-zoom-out-95 data-[state=open]:ds-zoom-in-95 data-[state=closed]:ds-slide-out-to-left-1/2 data-[state=closed]:ds-slide-out-to-top-[48%] data-[state=open]:ds-slide-in-from-left-1/2 data-[state=open]:ds-slide-in-from-top-[48%] sm:ds-max-w-lg dark:ds-border-gray-800 dark:ds-bg-gray-800',
         className,
       )}
       {...props}>
       {children}
       <DialogPrimitive.Close className="ds-absolute -ds-right-3 -ds-top-3 ds-inline-flex ds-size-9 ds-items-center ds-justify-center ds-rounded-full ds-border ds-border-gray-100 ds-bg-white ds-transition-colors hover:ds-bg-gray-50 dark:ds-border-gray-700 dark:ds-bg-gray-800 dark:hover:ds-bg-gray-700">
         <X className="ds-size-5 ds-text-gray-700 dark:ds-text-gray-50" />
-        <span className="ds-sr-only">Close</span>
+        <span className="ds-sr-only">닫기</span>
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
@@ -71,7 +71,7 @@ function DialogFooter({
   return (
     <div
       className={cn(
-        'ds-flex ds-flex-col-reverse sm:ds-flex-row sm:ds-justify-end sm:ds-space-x-2',
+        'ds-flex ds-items-center ds-justify-center ds-gap-2 sm:ds-justify-end',
         className,
       )}
       {...props}
@@ -102,7 +102,7 @@ const DialogDescription = React.forwardRef<
   <DialogPrimitive.Description
     ref={ref}
     className={cn(
-      'ds-break-keep ds-text-sm ds-text-gray-500 dark:ds-text-gray-400',
+      'ds-break-keep ds-text-gray-500 dark:ds-text-gray-400',
       className,
     )}
     {...props}
