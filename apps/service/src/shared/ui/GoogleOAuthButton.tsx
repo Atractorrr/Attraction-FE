16bae915988d9e5f503c7071800cf7c6419dede8
SWARVY <swarvy0826@naver.com>
2024-07-04 03:30:05 +0900

    fix: ios-pwa 감지 로직 임시 비활성화 (#ATR-540)

--- apps/service/src/shared/ui/GoogleOAuthButton.tsx
+++ apps/service/src/shared/ui/GoogleOAuthButton.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { GOOGLE_IOS_PWA_OAUTH_URL, GOOGLE_OAUTH_URL } from '../constant'
-import { useCheckDevice } from '../lib'
+import { GOOGLE_OAUTH_URL } from '../constant'
 
 interface GoogleLogoSVGProps {
   className?: string
@@ -45,11 +44,11 @@ function GoogleLogoSVG({ className }: GoogleLogoSVGProps) {
 }
 
 export default function GoogleOAuthButton() {
-  const { isIOSPWA } = useCheckDevice()
+  // const { isIOSPWA } = useCheckDevice()
 
   return (
     <Link
-      href={isIOSPWA ? GOOGLE_IOS_PWA_OAUTH_URL : GOOGLE_OAUTH_URL}
+      href={GOOGLE_OAUTH_URL}
       className="flex h-14 items-center gap-2 rounded-xl border border-gray-100 bg-white px-4 py-3 dark:border-gray-700 dark:bg-gray-700">
       <GoogleLogoSVG className="size-7" />
       <span className="mx-auto block whitespace-nowrap text-center xs:text-lg">
