f16ed634855359708c408d58c92801a66f6e6b1e
ChanLee_KR <cl700120@gmail.com>
2024-07-04 20:55:27 +0900

    refactor: useMemo 로 context api 리팩토링 (#ATR-544)

--- apps/service/src/shared/lib/util/index.ts
+++ apps/service/src/shared/lib/util/index.ts
@@ -1,6 +1,5 @@
 export { default as QueryProvider } from './QueryProvider'
 export { default as censoringAnchorTags } from './censoringAnchorTags'
 export { default as getTimeFromNow } from './getTimeFromNow'
-export * from './checkMobile'
 export { default as DeviceProvider, useCheckDevice } from './DeviceProvider'
 export { default as copy } from './copy'
