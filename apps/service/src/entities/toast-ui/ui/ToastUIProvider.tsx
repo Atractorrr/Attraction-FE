4b7f32af6b5d5bbee006759a7d9ee1c0075b7410
SWARVY <swarvy0826@naver.com>
2024-07-13 20:12:01 +0900

    feat: 새로 추가된 아이콘 반영 (#ATR-566)

--- apps/service/src/entities/toast-ui/ui/ToastUIProvider.tsx
+++ apps/service/src/entities/toast-ui/ui/ToastUIProvider.tsx
@@ -6,71 +6,24 @@ import { XOutline } from '@attraction/icons'
 import { PropsWithChildren } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import {
+  CheckCircleSolid,
+  ExclamationCircleSolid,
+  InformationCircleSolid,
+} from '@attraction/icons/src'
 
 const toastIcon = {
   success: (
-    <svg
-      // TODO: 아이콘 패키치 추가
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      className="m-auto text-xl text-green-400 dark:text-green-300"
-      viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10m-5.97-3.03a.75.75 0 0 1 0 1.06l-5 5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47l2.235-2.235L14.97 8.97a.75.75 0 0 1 1.06 0"
-        clipRule="evenodd"
-      />
-    </svg>
+    <CheckCircleSolid className="m-auto text-xl text-green-400 dark:text-green-300" />
   ),
   info: (
-    <svg
-      // TODO: 아이콘 패키치 추가
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      className="m-auto text-xl text-blue-400 dark:text-blue-300"
-      viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10m-10 5.75a.75.75 0 0 0 .75-.75v-6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75M12 7a1 1 0 1 1 0 2a1 1 0 0 1 0-2"
-        clipRule="evenodd"
-      />
-    </svg>
+    <InformationCircleSolid className="m-auto text-xl text-blue-400 dark:text-blue-300" />
   ),
   warning: (
-    <svg
-      // TODO: 아이콘 패키치 추가
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      className="m-auto text-xl text-yellow-400 dark:text-yellow-300"
-      viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10s10-4.477 10-10M12 6.25a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-1.5 0V7a.75.75 0 0 1 .75-.75M12 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2"
-        clipRule="evenodd"
-      />
-    </svg>
+    <ExclamationCircleSolid className="m-auto text-xl text-yellow-400 dark:text-yellow-300" />
   ),
   error: (
-    <svg
-      // TODO: 아이콘 패키치 추가
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      className="m-auto text-xl text-red-400 dark:text-red-300"
-      viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10s10-4.477 10-10M12 6.25a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-1.5 0V7a.75.75 0 0 1 .75-.75M12 17a1 1 0 1 0 0-2a1 1 0 0 0 0 2"
-        clipRule="evenodd"
-      />
-    </svg>
+    <ExclamationCircleSolid className="m-auto text-xl text-red-400 dark:text-red-300" />
   ),
   default: null,
 }
