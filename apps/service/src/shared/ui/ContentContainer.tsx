571f94897cb94bc004bf34cb9d83a0746c7b663b
ChanLee_KR <cl700120@gmail.com>
2024-07-24 14:35:52 +0900

    refactor: 사용하지 않는 컴포넌트 제거 (#ATR-577)

--- apps/service/src/shared/ui/ContentContainer.tsx
+++
@@ -1,5 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-
-export default function ContentContainer({ children }: PropsWithChildren) {
-  return <div className="mt-3 flex gap-2">{children}</div>
-}
