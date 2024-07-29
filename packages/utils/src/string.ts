0750e80ad7ec9c218f9a4e7cbb9bcf279a99e952
SWARVY <swarvy0826@naver.com>
2024-07-29 15:46:25 +0900

    chore: 함수의 기능을 명확하게 표현하기 위해 cn, clsx 함수 이름 변경 (#ATR-589)

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,10 +1,10 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function twClassNames(...inputs: ClassValue[]) {
+export function twcn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function classNames(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return clsx(inputs)
 }
