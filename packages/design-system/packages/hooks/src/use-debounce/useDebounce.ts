d36c63f15679ab3ff93e4f9a7a49f47816f4e74a
ChanLee_KR <cl700120@gmail.com>
2024-08-02 22:44:47 +0900

    refactor: use debounce 테스트 코드 작성 및 리팩토링 (#ATR-600)

--- packages/design-system/packages/hooks/src/use-debounce/useDebounce.ts
+++ packages/design-system/packages/hooks/src/use-debounce/useDebounce.ts
@@ -4,11 +4,11 @@ export default function useDebounce<T>(value: T, delay: number) {
   const [debouncedValue, setDebouncedValue] = React.useState<T>(value)
 
   React.useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setDebouncedValue(value)
-    }, delay)
+    const timeoutId = setTimeout(() => setDebouncedValue(value), delay)
 
-    return () => clearTimeout(timeoutId)
+    return () => {
+      clearTimeout(timeoutId)
+    }
   }, [value, delay])
 
   return debouncedValue
