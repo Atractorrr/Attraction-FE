8ab632026516af7fa11500c142ecf2d6e861bed0
ChanLee_KR <cl700120@gmail.com>
2024-08-02 22:02:39 +0900

    chore: 공용 훅 패키지 테스트 환경 세팅 및 스크립트 추가 (#ATR-591)

---
+++ packages/design-system/packages/hooks/vitest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      reporter: ['text'],
+    },
+  },
+})
