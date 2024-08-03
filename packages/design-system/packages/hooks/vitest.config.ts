e403ad283685371a30c7cff6551c56d76a3356fc
ChanLee_KR <cl700120@gmail.com>
2024-08-03 16:53:12 +0900

    chore: user event 테스트 라이브러리 및 vitest react 플러그인 설치 (#ATR-591)

--- packages/design-system/packages/hooks/vitest.config.ts
+++ packages/design-system/packages/hooks/vitest.config.ts
@@ -1,7 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
