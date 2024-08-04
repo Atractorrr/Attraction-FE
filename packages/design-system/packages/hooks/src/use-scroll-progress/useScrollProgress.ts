865498e7551467c5e5d5f4f5d3654391bcd50060
ChanLee_KR <cl700120@gmail.com>
2024-08-04 19:32:27 +0900

    refactor: use scroll progress 훅 테스트코드 작성 및 리팩토링 (#ATR-600)

--- packages/design-system/packages/hooks/src/use-scroll-progress/useScrollProgress.ts
+++ packages/design-system/packages/hooks/src/use-scroll-progress/useScrollProgress.ts
@@ -2,14 +2,14 @@ import React from 'react'
 import { useWindowEvent } from '../use-window-event'
 
 export default function useScrollProgress() {
-  const [scrollProgress, setScrollProgress] = React.useState(0)
+  const [progress, setProgress] = React.useState(0)
 
   useWindowEvent('scroll', () => {
     const currentScroll = window.scrollY
     const totalHeight = document.body.scrollHeight - window.innerHeight
-    const progress = Math.round((currentScroll / totalHeight) * 100)
-    setScrollProgress(progress)
+    const scrollProgress = Math.round((currentScroll / totalHeight) * 100)
+    setProgress(scrollProgress)
   })
 
-  return { scrollProgress }
+  return { progress }
 }
