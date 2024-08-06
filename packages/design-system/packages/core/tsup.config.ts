7042cb6e8e124e9a768d8a1dbecd75e8c4704fce
ChanLee_KR <cl700120@gmail.com>
2024-08-06 22:43:32 +0900

    chore: ds core 패키지에 scss 설치 및 세팅 (#ATR-591)

--- packages/design-system/packages/core/tsup.config.ts
+++ packages/design-system/packages/core/tsup.config.ts
@@ -1,13 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Options } from 'tsup'
+import { sassPlugin } from 'esbuild-sass-plugin'
 
 const options: Options = {
   format: ['cjs', 'esm'],
   clean: true,
   sourcemap: 'inline',
   dts: true,
-  entryPoints: ['src/index.ts'],
+  entryPoints: ['src/index.ts', 'src/index.scss'],
   external: ['react', 'react-dom'],
   minify: true,
+  esbuildPlugins: [sassPlugin({ type: 'css' })],
 }
 
 export default options
