3b32a439e5b1b394bc65b66041984d82b5f55d66
ChanLee_KR <cl700120@gmail.com>
2024-08-09 17:11:35 +0900

    chore: sass 빌드 방식 변경 및 esbuild sass 플러그인 제거 (#ATR-591)

--- packages/design-system/packages/core/tsup.config.ts
+++ packages/design-system/packages/core/tsup.config.ts
@@ -1,16 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { Options } from 'tsup'
-import { sassPlugin } from 'esbuild-sass-plugin'
 
 const options: Options = {
   format: ['cjs', 'esm'],
   clean: true,
   sourcemap: 'inline',
   dts: true,
-  entryPoints: ['src/index.ts', 'src/index.scss'],
+  entryPoints: ['src/index.ts'],
   external: ['react', 'react-dom'],
   minify: true,
-  esbuildPlugins: [sassPlugin({ type: 'css' })],
 }
 
 export default options
