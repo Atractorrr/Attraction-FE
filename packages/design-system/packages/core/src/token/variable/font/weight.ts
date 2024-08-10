77a534ad62b61ce3e7ec5eea6010694d6856a16b
ChanLee_KR <cl700120@gmail.com>
2024-08-10 19:53:56 +0900

    refactor: 디자인 토큰 정리 및 변수화 (#ATR-602)

---
+++ packages/design-system/packages/core/src/token/variable/font/weight.ts
@@ -0,0 +1,3 @@
+export const regular = 'var(--attraction-ds-font-weight-regular)'
+export const medium = 'var(--attraction-ds-font-weight-medium)'
+export const bold = 'var(--attraction-ds-font-weight-bold)'
