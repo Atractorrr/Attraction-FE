f14a5666f5c414f2b3579f5630fb644f794ecc76
woogie0303 <rkdehddnr15123@gmail.com>
2024-08-17 13:48:02 +0900

    feat: 모달 수정 (#ATR-590)

--- apps/service/src/entities/modal/ui/ModalController.tsx
+++ apps/service/src/entities/modal/ui/ModalController.tsx
@@ -1,12 +1,6 @@
 'use client'
 
-import {
-  forwardRef,
-  useCallback,
-  useEffect,
-  useImperativeHandle,
-  useState,
-} from 'react'
+import { forwardRef, useCallback, useImperativeHandle, useState } from 'react'
 import { CreateModalElement } from '../model'
 
 interface ModalPropsType {
@@ -18,7 +12,7 @@ export default forwardRef(function ModalController(
   { modalElement: ModalElement, exitFromModalArr }: ModalPropsType,
   ref,
 ) {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(true)
   const handleModalClose = useCallback(() => {
     setIsOpen(false)
     // 메모리에서 완전히 모달을 삭제 하기 위함이고 아래 코드를 추가 안하면 모달 animation이 바로 적용이 안된다.
@@ -34,10 +28,6 @@ export default forwardRef(function ModalController(
     },
     [handleModalClose],
   )
-
-  useEffect(() => {
-    setIsOpen(true)
-  }, [])
 
   return <ModalElement isOpen={isOpen} close={handleModalClose} />
 })
