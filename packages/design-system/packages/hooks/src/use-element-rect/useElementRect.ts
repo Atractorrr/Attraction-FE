34064244b6e855219bcaf5eed0aa0c08a57032d4
ChanLee_KR <cl700120@gmail.com>
2024-08-26 18:21:34 +0900

    refactor: use element rect 훅 추가 (#ATR-597)

---
+++ packages/design-system/packages/hooks/src/use-element-rect/useElementRect.ts
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useWindowEvent } from '../use-window-event'
+
+const defaultRect = {
+  width: 0,
+  height: 0,
+  x: 0,
+  y: 0,
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+}
+
+function getElementRect<T extends HTMLElement>(element?: T | null) {
+  if (!element) {
+    return defaultRect
+  }
+  return element?.getBoundingClientRect() ?? defaultRect
+}
+
+export default function useElementRect<T extends HTMLElement>(
+  ref: React.MutableRefObject<T | null>,
+) {
+  const [rect, setRect] = React.useState(getElementRect(ref.current))
+  const handleResize = React.useCallback(() => {
+    setRect(getElementRect(ref.current))
+  }, [ref.current])
+
+  React.useEffect(() => handleResize(), [])
+
+  useWindowEvent('resize', handleResize)
+  useWindowEvent('scroll', handleResize)
+
+  return rect
+}
