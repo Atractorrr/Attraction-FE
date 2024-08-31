8b4855156dda8a64e16b53a9c1b7346f2dbee1ba
ChanLee_KR <cl700120@gmail.com>
2024-08-31 18:53:13 +0900

    refactor: hidden text 컴포넌트 forward ref 제네릭 타입 적용 (#ATR-603)

--- packages/design-system/packages/core/src/components/hidden-text/HiddenText.tsx
+++ packages/design-system/packages/core/src/components/hidden-text/HiddenText.tsx
@@ -1,37 +1,36 @@
 import React from 'react'
+import { forwardRefWithGeneric } from '../../core'
 
 type HiddenTextProps<T extends React.ElementType> = {
   as?: T
 } & React.ComponentPropsWithoutRef<T>
 
-export const HiddenText = React.forwardRef(
-  <T extends React.ElementType = 'span'>(
-    { as, style, ...props }: HiddenTextProps<T>,
-    ref: React.ComponentPropsWithRef<T>['ref'],
-  ) => {
-    const Component = as || 'span'
+function HiddenText<T extends React.ElementType = 'span'>(
+  { as, style = {}, ...props }: HiddenTextProps<T>,
+  ref?: React.ComponentPropsWithRef<T>['ref'],
+) {
+  const Component = as || 'span'
 
-    return (
-      <Component
-        ref={ref}
-        style={{
-          position: 'absolute',
-          width: '1px',
-          height: '1px',
-          padding: '0px',
-          margin: '0px',
-          border: 'none',
-          fontSize: '2px',
-          whiteSpace: 'nowrap',
-          clip: 'rect(1px 1px 1px 1px)',
-          outline: 'none',
-          overflow: 'hidden',
-          ...style,
-        }}
-        {...props}
-      />
-    )
-  },
-)
+  return (
+    <Component
+      ref={ref}
+      style={{
+        position: 'absolute',
+        width: '1px',
+        height: '1px',
+        padding: '0px',
+        margin: '0px',
+        border: 'none',
+        fontSize: '2px',
+        whiteSpace: 'nowrap',
+        clip: 'rect(1px 1px 1px 1px)',
+        outline: 'none',
+        overflow: 'hidden',
+        ...style,
+      }}
+      {...props}
+    />
+  )
+}
 
-export default HiddenText
+export default forwardRefWithGeneric(HiddenText)
