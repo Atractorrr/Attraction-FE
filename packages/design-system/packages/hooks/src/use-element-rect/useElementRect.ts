0e47e73ec5063cab9d7cb26d5be7fafe569c3fce
ChanLee_KR <cl700120@gmail.com>
2024-09-02 00:33:38 +0900

    refactor: use element rect 훅 반환 타입 수정 (#ATR-597)

--- packages/design-system/packages/hooks/src/use-element-rect/useElementRect.ts
+++ packages/design-system/packages/hooks/src/use-element-rect/useElementRect.ts
@@ -1,28 +1,27 @@
 import React from 'react'
 import { useWindowEvent } from '../use-window-event'
 
-const defaultRect: Omit<DOMRect, 'toJSON'> = {
-  width: 0,
-  height: 0,
-  x: 0,
-  y: 0,
+type ElementRect = Readonly<Omit<DOMRect, 'x' | 'y' | 'toJSON'>>
+
+const defaultRect: ElementRect = Object.freeze({
   top: 0,
   bottom: 0,
   left: 0,
   right: 0,
-}
+  width: 0,
+  height: 0,
+})
 
-function getElementRect<T extends HTMLElement>(element?: T | null) {
-  if (!element) {
-    return defaultRect
-  }
+function getElementRect<T extends HTMLElement>(
+  element?: T | null,
+): ElementRect {
   return element?.getBoundingClientRect() ?? defaultRect
 }
 
 export default function useElementRect<T extends HTMLElement>(
   ref: React.MutableRefObject<T | null>,
   deps?: React.DependencyList,
-) {
+): ElementRect {
   const [rect, setRect] = React.useState(getElementRect(ref.current))
   const handleResize = React.useCallback(() => {
     setRect(getElementRect(ref.current))
