73bd37075be836df940d399123c15aab3e45ee98
ChanLee_KR <cl700120@gmail.com>
2024-09-05 00:21:46 +0900

    refactor: forward ref with generic 유틸 함수 타입 수정 (#ATR-597)

--- packages/design-system/packages/core/src/core/utils/forwardRefWithGeneric.ts
+++ packages/design-system/packages/core/src/core/utils/forwardRefWithGeneric.ts
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function forwardRefWithGeneric<T, P = {}>(
   render: (props: P, ref: React.Ref<T>) => React.ReactNode,
-): (props: P & React.RefAttributes<T>) => React.ReactNode {
-  return React.forwardRef(render) as any
+) {
+  return React.forwardRef(render) as (
+    props: P & React.RefAttributes<T>,
+  ) => React.ReactNode
 }
