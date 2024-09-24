e66d44adb381ec61cc49963927325e82f555647e
woogie0303 <rkdehddnr15123@gmail.com>
2024-09-24 13:00:16 +0900

    feat: notice header 기능 추가(#ATR-610)

--- apps/service/src/features/notice/ui/NoticeHeader.tsx
+++ apps/service/src/features/notice/ui/NoticeHeader.tsx
@@ -1,16 +1,29 @@
 'use client'
 
 import { Button } from '@attraction/design-system'
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import { NOTICE_NAVIGATION } from '../constant'
 
 export default function NoticeHeader() {
+  const searchParam = useSearchParams()
+  const noticeUrlParamsCategory = searchParam.get('category')
+  const pageNum = searchParam.get('page')
+
   return (
     <div className="flex w-fit rounded-lg bg-gray-50 p-1">
-      {NOTICE_NAVIGATION.type.map((noticeType) => (
-        <Button className="rounded px-6 py-1 hover:bg-white">
-          {noticeType}
-        </Button>
-      ))}
+      {NOTICE_NAVIGATION.type.map((noticeType) => {
+        const noticeCategoryType =
+          NOTICE_NAVIGATION[noticeType as keyof typeof NOTICE_NAVIGATION]
+        return (
+          <Link href={`/notice?category=${noticeCategoryType}&page=${pageNum}`}>
+            <Button
+              className={`rounded px-6 py-1 hover:bg-white ${noticeUrlParamsCategory === noticeCategoryType ? 'bg-white' : ''}`}>
+              {noticeType}
+            </Button>
+          </Link>
+        )
+      })}
     </div>
   )
 }
