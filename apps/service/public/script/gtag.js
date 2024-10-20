32760e17d224bcd7c538cd34cfcd264495c67a4a
ChanLee_KR <chanlee1007@naver.com>
2024-10-20 20:23:32 +0900

    베타 서비스 배너 및 gtag 적용 (#124)
    * feat: 베타 서비스 오픈 배너 적용 (#ATR-617)
    
    * feat: gtag 연결 (#ATR-618)

---
+++ apps/service/public/script/gtag.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+function gtag() {
+  dataLayer.push(arguments)
+}
+gtag('js', new Date())
+
+gtag('config', 'G-1WN6EE9VE8')
--- apps/service/src/widgets/main/ui/MainAds.tsx
+++ apps/service/src/widgets/main/ui/MainAds.tsx
@@ -4,9 +4,9 @@ export default function MainAds() {
   return (
     <div className="px-5 md:px-0">
       <Image
-        src="/images/ads.jpg"
-        width={1500}
-        height={1100}
+        src="/images/beta-service-banner.jpg"
+        width={1680}
+        height={640}
         className="h-auto w-full rounded-2xl object-cover"
         alt="ads"
       />
